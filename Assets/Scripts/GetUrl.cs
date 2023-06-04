4222cc26763bd775cb247712b865ed6c471515a1
Henrique Monteiro <henrique.m.silva1@gmail.com>
2023-06-04 16:52:17 -0300

    add reference geturl

--- Assets/Scripts/GetUrl.cs
+++ Assets/Scripts/GetUrl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class GetUrl : MonoBehaviour
 { 
-    private static string urlAbsolute;
+    private string urlAbsolute;
     public delegate void UrlLoadedHandler(string url);
     public event UrlLoadedHandler UrlLoaded;
     
@@ -17,10 +17,7 @@ public class GetUrl : MonoBehaviour
     void GetAbsoluteUrl()
     {
         urlAbsolute = Application.absoluteURL;
-        if(UrlLoaded != null)
-        {
-            UrlLoaded(urlAbsolute);
-        }
+        UrlLoaded?.Invoke(urlAbsolute);
     }
 
 }
